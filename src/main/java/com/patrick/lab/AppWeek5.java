44c8427cec5d8ebbf21306ef3ef91dc7d37246fa
Patrick <psoumani@gmail.com>
2021-02-04 23:56:34 -0500

    Added the method getIntersection() and its unit test

--- src/main/java/com/patrick/lab/AppWeek5.java
+++ src/main/java/com/patrick/lab/AppWeek5.java
@@ -6,6 +6,7 @@ public class AppWeek5 {
 
     public static void main(String[] args) {
         
+        //Execution of the method getAverage
         int a = 2;
         int b = 4;
         int c = 10;
@@ -13,6 +14,7 @@ public class AppWeek5 {
         AppWeek5 nAppWeek5 = new AppWeek5();
         nAppWeek5.getAverage(a,b,c);
 
+        //Execution of the method sumUpTarget
         ArrayList<Integer> numList = new ArrayList<>();
         numList.add(5);
         numList.add(7);
@@ -26,6 +28,27 @@ public class AppWeek5 {
         int goal = 10;
 
         nAppWeek5.sumUpTarget(numList, goal);
+
+        //Execution of the method returnIntersection
+        ArrayList<Integer> firstList = new ArrayList<>();
+        firstList.add(1);
+        firstList.add(2);
+        firstList.add(3);
+        firstList.add(2); 
+        firstList.add(5);
+        firstList.add(6);
+        
+        ArrayList<Integer> secondList = new ArrayList<>();
+        secondList.add(0);
+        secondList.add(2);
+        secondList.add(4);
+        secondList.add(6); 
+        secondList.add(8);
+        secondList.add(10);
+
+        nAppWeek5.getIntersection(firstList, secondList);
+
+
     }
 
     public int getAverage(int num1, int num2, int num3) {
@@ -39,7 +62,7 @@ public class AppWeek5 {
         return avg;
     }
 
-    public boolean sumUpTarget(ArrayList<Integer>numberList, int target) {
+    public boolean sumUpTarget(ArrayList<Integer> numberList, int target) {
         int sum = target;
         boolean result = false;
 
@@ -53,5 +76,23 @@ public class AppWeek5 {
         System.out.println("There are two numbers in the array that sum up to " +target +" : " +result);
         return result;
         
+    }
+
+    public ArrayList<Integer> getIntersection(ArrayList<Integer> listA, ArrayList<Integer> listB) {
+        int temp;
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+        for (int i = 0; i < listA.size(); i++){
+            temp = listA.get(i);
+            if ((listB.contains(temp) == true) && (resultList.contains(temp) == false)) {
+                resultList.add(temp);
+            }
+        }
+        if (resultList.isEmpty() == true) {
+            System.out.println("There is no intersection between both lists");
+        } else {
+            System.out.println("The intersection between the two initial lists is " +resultList);
+        }
+    return resultList;
     }
 }
--- src/test/java/com/patrick/lab/AppWeek5Test.java
+++ src/test/java/com/patrick/lab/AppWeek5Test.java
@@ -24,7 +24,39 @@ public class AppWeek5Test {
         testList.add(2);
         testList.add(0); 
         testList.add(6);
+
         assertEquals(true, nAppweek5.sumUpTarget(testList, 6));
         
+    }
+
+    @Test
+    public void returnIntersection() {
+        AppWeek5 nAppWeek5 = new AppWeek5();
+        ArrayList<Integer> testListA = new ArrayList<>();
+        testListA.add(1);
+        testListA.add(2);
+        testListA.add(3);
+        testListA.add(2); 
+        testListA.add(5);
+        testListA.add(6);
+        
+        ArrayList<Integer> testListB = new ArrayList<>();
+        testListB.add(1);
+        testListB.add(3);
+        testListB.add(5);
+        testListB.add(7); 
+        testListB.add(2);
+        testListB.add(10);
+
+        ArrayList<Integer> resultExpected = new ArrayList<>();
+        resultExpected.add(1);
+        resultExpected.add(2);
+        resultExpected.add(3);
+        resultExpected.add(5);
+
+        ArrayList<Integer> resultActual = new ArrayList<>();
+        resultActual = nAppWeek5.getIntersection(testListA, testListB);
+
+        assertEquals(resultExpected,resultActual);
     }
 }
